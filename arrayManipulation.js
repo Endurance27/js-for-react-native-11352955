06e832bf8acf1cd879b903ee2d3ec9a8e34cee1c
Endykay23 <offeibeaendurance1@gmail.com>
2024-05-17 16:51:50 +0000

    Task one done

---
+++ arrayManipulation.js
@@ -0,0 +1,9 @@
+// arrayManipulation.js
+function processArray(arr) {
+    return arr.map(num => num % 4 === 0 ? num * num : num * 3);
+  }
+  const inputArray = [1, 2, 3, 4, 5, 6];
+  const resultArray = processArray(inputArray);
+  
+  console.log("Input Array:", inputArray);
+  console.log("Result Array:", resultArray);
