862c1329803a090b35685706992f5fabb1f20d51
Endurance27 <eoasabadu@st.ug.edu.gh>
2024-05-18 12:49:08 +0000

    task 3 is done

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,52 @@ function processArray(arr) {
   console.log("Result Array:", resultArray);
 
 
+
+  function formatArrayStrings(stringArray, numberArray) {
+    if (stringArray.length !== numberArray.length) {
+        throw new Error('Both arrays must have the same length');
+    }
+
+    return stringArray.map((str, index) => {
+        const num = numberArray[index];
+        if (num % 4 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+// Example usage:
+const strings = ["Endurance", "Adel", "cherry", "kiwi","apple"];
+const numbers = [2, 3, 4, 5, 6];
+
+const formattedStrings = formatArrayStrings(strings, numbers);
+console.log(formattedStrings); // Output: "Endurance", "Adel", "cherry", "kiwi","apple"]
+
+
+
+
+
+
+// Example input arrays
+const names = ["Kofi", "Adel", "Esi"];
+const modifiedNames = ["Emily", "Manuel", "Abena"];
+
+function createUserProfiles(names, modifiedNames) {
+  const userProfiles = [];
+  for (let i = 0; i < names.length; i++) {
+    const userProfile = {
+      originalName: names[i],
+      modifiedName: modifiedNames[i],
+      id: i + 1,
+    };
+    userProfiles.push(userProfile);
+  }
+  return userProfiles;
+}
+
+// Example usage
+const profiles = createUserProfiles(names, modifiedNames);
+console.log(profiles);
+
