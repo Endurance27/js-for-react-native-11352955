262300a6dc8a398ac2e0174f26088b31811eed6e
Endurance27 <eoasabadu@st.ug.edu.gh>
2024-05-18 12:27:47 +0000

    done with task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,25 +9,3 @@ function processArray(arr) {
   console.log("Result Array:", resultArray);
 
 
-
-function formatArrayStrings(stringArray, numberArray) {
-    if (stringArray.length !== numberArray.length) {
-        throw new Error('Both arrays must have the same length');
-    }
-
-    return stringArray.map((str, index) => {
-        const num = numberArray[index];
-        if (num % 4 === 0) {
-            return str.toUpperCase();
-        } else {
-            return str.toLowerCase();
-        }
-    });
-}
-
-// Example usage:
-const strings = ["Endurance", "Adel", "cherry", "kiwi","apple"];
-const numbers = [2, 3, 4, 5, 6];
-
-const formattedStrings = formatArrayStrings(strings, numbers);
-console.log(formattedStrings); // Output: "Endurance", "Adel", "cherry", "kiwi","apple"]
